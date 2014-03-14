e48127654c9868d6f58f8d462e7a75764843a40b
bhsaurabh <srbhatia10@gmail.com>
2014-03-14 13:46:47 +0530

    Working solution

--- StackEmUp.java
+++ StackEmUp.java
@@ -25,7 +25,7 @@ public class Main implements Runnable {
 		/* Takes 52 integers as input showing the particular shuffle */
 		private void getShuffleArray() {
 			for (int j = 0; j < n; j++) 
-				for (int i = 0; i < 52; i++)	shuffle[j][i] = sc.nextInt();
+				for (int i = 0; i < 52; i++)	shuffle[j][i] = sc.nextInt()-1;
 		}
 		
 		/* Shuffle the deck */
@@ -35,11 +35,55 @@ public class Main implements Runnable {
 			int[] cardsCopy = new int[52];
 			System.arraycopy(cards, 0, cardsCopy, 0, cards.length);	// not re-inventing the wheel here
 			for (int j = 0; j < 52; j++) {
-				int i = shuffle[num][j];
+				int i = shuffle[num-1][j];  // as array indexing starts from 0
 				// i in position j means that ith card in deck moved to position j
+                cards[j] = cardsCopy[i];    // this is our final array
 			}
 		}
 		
+        /* Display the final cards stack as per problem */
+        private void displayFinalStack() {
+            for (int j = 0; j < 52; j++) {
+                int i = cards[j]+1;
+                if (i <= 13) {
+                    // clubs is the suite
+                    if (i == 10)    System.out.print("Jack ");
+                    else if (i == 11)   System.out.print("Queen ");
+                    else if (i == 12)   System.out.print("King ");
+                    else if (i == 13)   System.out.print("Ace ");
+                    else    System.out.print((i+1)%13 + " ");
+                    System.out.println("of Clubs");
+                }
+                else if (i <= 26) {
+                    // Diamonds is the suite
+                    if (i == 23)    System.out.print("Jack ");
+                    else if (i == 24)   System.out.print("Queen ");
+                    else if (i == 25)   System.out.print("King ");
+                    else if (i == 26)   System.out.print("Ace ");
+                    else    System.out.print((i+1)%13 + " ");
+                    System.out.println("of Diamonds");
+                }
+                else if (i <= 39) {
+                    // Hearts is the suite
+                    if (i == 36)    System.out.print("Jack ");
+                    else if (i == 37)   System.out.print("Queen ");
+                    else if (i == 38)   System.out.print("King ");
+                    else if (i == 39)   System.out.print("Ace ");
+                    else    System.out.print((i+1)%13 + " ");
+                    System.out.println("of Hearts");
+                }
+                else {
+                    // Spades is the suite
+                    if (i == 49)    System.out.print("Jack ");
+                    else if (i == 50)   System.out.print("Queen ");
+                    else if (i == 51)   System.out.print("King ");
+                    else if (i == 52)   System.out.print("Ace ");
+                    else    System.out.print((i+1)%13 + " ");
+                    System.out.println("of Spades");
+                }
+            }
+        }
+
 		/* Runnable's abstract method */
 		public void run() {
 			// take number of possible shuffles
@@ -52,6 +96,7 @@ public class Main implements Runnable {
 				int shuffleNum = sc.nextInt();
 				performShuffle(shuffleNum);
 			}
+            displayFinalStack();
 		}
 		
 	}
@@ -64,6 +109,16 @@ public class Main implements Runnable {
 	
 	/* Abstract method from Runnable */
 	public void run() {
-		
+	    // get number of test cases
+        int n = sc.nextInt();
+        sc.nextLine();  // to take on blank line
+        for (int i = 0; i < n; i++) {
+            Deck deck = new Deck();
+            deck.run();
+            if (i != n-1) {
+                System.out.println();
+                sc.nextLine();
+            }
+        }
 	}
 }
